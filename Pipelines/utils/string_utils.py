d3aa8daeac44428212206fdf1e13153975145d5a
Abrok14 <abrokmatej15@gmail.com>
2026-05-21 10:16:17 +0200

    abrok verifikacia spravneho emailu

--- Pipelines/utils/string_utils.py
+++ Pipelines/utils/string_utils.py
@@ -1,4 +1,4 @@
-
+import re
 
 def concat_strings(str1: str, str2: str) -> str|None:
     """Concatenates two strings.
@@ -9,7 +9,23 @@ def concat_strings(str1: str, str2: str) -> str|None:
 
     Returns:
         str|None: concatenated string
-    """
+    """ 
     if not isinstance(str1, str) or not isinstance(str2, str):
         return None
     return str1 + str2
+
+
+def is_valid_email(email: str) -> bool:
+
+    if not isinstance(email, str):
+        return False
+    if "@" not in email:
+        return False
+    local, domain = email.split("@") #rozdeli email na local a domain = [local@domain]
+    if not local or not domain:
+        return False
+    if domain.startswith("."):
+        return False
+    if not re.search(r'\.[a-zA-Z]{2,}$', domain): #re.search hlada ci je v domain .com .sk atd...
+        return False
+    return True
