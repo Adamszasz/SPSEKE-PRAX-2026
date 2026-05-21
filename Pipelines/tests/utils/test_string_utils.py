84561dd8679cba103a448960f92962bb8009f9ec
peter <peter.szasz333@gmail.com>
2026-05-21 09:32:55 +0200

    update gitignore and add function clean_to_title_case and test to clean_to_title_case

--- Pipelines/tests/utils/test_string_utils.py
+++ Pipelines/tests/utils/test_string_utils.py
@@ -1,6 +1,7 @@
 
     
 from Pipelines.utils.string_utils import concat_strings
+from Pipelines.utils.string_utils import clean_to_title_case
 
 
 class TestConcatStrings:
@@ -16,3 +17,29 @@ class TestConcatStrings:
     def test_invalid_input(self): 
         result = concat_strings("Hello, ", 123)
         assert result == None
+
+class TestCleanToTitleCase:
+    
+    def test_valid_input(self):
+        result = clean_to_title_case("  hello world  ")
+        assert result == "Hello World"
+
+    def test_empty_string(self):
+        result = clean_to_title_case("")
+        assert result == ""
+
+    def test_invalid_input(self):
+        result = clean_to_title_case(123)
+        assert result == None
+
+    def test_messy_input(self):
+        result = clean_to_title_case("  hELLO wORLD  ")
+        assert result == "Hello World"
+
+    def test_already_title_case(self):
+        result = clean_to_title_case("Hello World")
+        assert result == "Hello World"
+
+    def test_mixed_case(self):
+        result = clean_to_title_case("  it's a beautiful day  ")
+        assert result == "It's A Beautiful Day"
