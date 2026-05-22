839f13ca759233f91b58414de88a4d149d702712
Abrok14 <abrokmatej15@gmail.com>
2026-05-22 09:18:01 +0200

    abrok_emaill

--- Pipelines/utils/string_utils.py
+++ Pipelines/utils/string_utils.py
@@ -15,17 +15,18 @@ def concat_strings(str1: str, str2: str) -> str|None:
     return str1 + str2
 
 
-def is_valid_email(email: str) -> bool:
+email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
+
+def is_valid_email(email: str) -> bool:
+    """Validates email format using regex pattern.
+
+    Args:
+        email (str): email address to validate
+
+    Returns:
+        bool: True if email is valid, False otherwise
+    """
     if not isinstance(email, str):
         return False
-    if "@" not in email:
-        return False
-    local, domain = email.split("@") #rozdeli email na local a domain = [local@domain]
-    if not local or not domain:
-        return False
-    if domain.startswith("."):
-        return False
-    if not re.search(r'\.[a-zA-Z]{2,}$', domain): #re.search hlada ci je v domain .com .sk atd...
-        return False
-    return True
+    return bool(re.fullmatch(email_regex, email))
